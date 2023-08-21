78c81c8f5ceb37974a0107bd1560f3ab53e110fe
Rachel King <rachel.b.king@gmail.com>
2023-08-21 18:48:45 -0400

    Tentative final version of program.
    main.py is pretty sloppy but I think I'd have to get significantly more
    sophisticated to manage the fake-streaming-that-isn't-actually-streaming
    and the fake-present-time-that-isn't-actually-the-present time.
    
    sample_output.txt is what this program currently outputs for the log file I
    was sent in the take home instructions.

--- structured_log_alerting/main.py
+++ structured_log_alerting/main.py
@@ -20,7 +20,8 @@ def main():
     # once gets me the perks of an iterable (specifically, the combo
     # of line buffering + line tracking with the power of #__next__())
     # and i'm not sure how to get that without basically reimplementing
-    # this as a class that manually reinvents all of that.
+    # this as a class that manually reinvents all of that or using something
+    # more formal like asyncio to handle file opening closing.
     with open(args.file_location) as f:
         reader = csv.DictReader(f)
 
@@ -35,14 +36,28 @@ def main():
         for line in reader:
             try:
                 metric_name, parsed_log_line = parser.parse_log_line(line)
-                # print(metric_name)
-                # print(parsed_log_line)
                 counters_collection.add_or_update_series(metric_name, parsed_log_line)
                 log_timestamp = parser.parse_timestamp(line)
 
                 if log_timestamp > current_time:
+                    # our only (and therefore best) proxy of "the present" is
+                    # just whatever latest timestamp we've ever seen. if we
+                    # do see a later timestamp, we can assume "the present"
+                    # has moved forward. but that's the best info we've got.
                     current_time = log_timestamp
+                    alert_message = alertmanager.check_for_elevated_requests(
+                        current_time
+                    )
+                    if len(alert_message) > 0:
+                        print(alert_message)
 
+                # all of this timekeeping is clumsy but also feels good
+                # enough. i think my next step would be something like a
+                # pointer or two stored on disk if/when we started treating
+                # this like a proper tsdb that stored other things on disk.
+                # a persistent pointer also starts to feel like yet another
+                # place where we actually have a producer/consumer model
+                # like a queue.
                 if start_of_current_ten_second_interval == 0:
                     start_of_current_ten_second_interval = current_time
 
@@ -50,6 +65,7 @@ def main():
                     start_of_current_ten_second_interval + ten_seconds_in_timedelta
                     <= current_time
                 ):
+                    # if it's been 10+ seconds since our last summary:
                     summary = alertmanager.provide_summary_for_interval(current_time)
                     for line in summary:
                         print(line)
